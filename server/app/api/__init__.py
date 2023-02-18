7f65c59aed4d1898e95f0a91fc083db4814d2575
mikezm <mike.marshall6@gmail.com>
2023-02-18 11:26:40 -0500

    refactor API

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -2,12 +2,13 @@
 py-stream-cast API
 
 """
-from app.config import CHROME_CAST_UUID, CAST_STREAM_ADDRESS
+from app.config import CHROME_CAST_UUID, CAST_STREAM_ADDRESS, API_BASE
 from app.services.chromecast import Casts
 from app.services.audio import Stream
 from flask import Flask, Response, jsonify
 
 app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = API_BASE
 casts = Casts()
 stream = Stream()
 
@@ -23,9 +24,9 @@ def list_casts():
     return jsonify(casts.list_chromecasts()), 200
 
 
-@app.route("/start-cast")
-def start_cast():
-    casts.select_cast_device(CHROME_CAST_UUID)
+@app.route("/start-cast/<uuid:id>")
+def start_cast(id):
+    casts.select_cast_device(id)
     casts.play("http://{}/audio-stream".format(CAST_STREAM_ADDRESS), "audio/x-wav")
     return Response(None, 200)
 
--- server/app/config/__init__.py
+++ server/app/config/__init__.py
@@ -15,11 +15,13 @@ def get_local_ip():
 
 
 RUN_ENV = os.getenv("PI_STREAM_CAST_ENV", "dev")
+API_BASE = ""
 
 # ip address of the Raspberry Pi on your local network
 CAST_STREAM_ADDRESS = get_local_ip()
 if RUN_ENV == "dev":
     CAST_STREAM_ADDRESS += ":5000"
+    API_BASE = "/api"
 
 CHROME_CAST_UUID = "a050f03a-0faa-9fec-022c-2c0dd8f4dc65"
 
